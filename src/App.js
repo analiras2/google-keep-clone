da169cf564b6b26c9b279e2a26d5bc349a19711f
Analira Scalabrini <analira.scalabrini@gmail.com>
2021-07-02 12:33:39 -0400

    Header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import Header from './components/Header';
 
 function App(props) {
-  return <div>Hello Word</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
